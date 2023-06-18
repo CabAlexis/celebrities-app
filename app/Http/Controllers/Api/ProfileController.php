43c15eb81109ec3281e4d3c82412dd0202b72fac
CabAlexis <alexis.cabillic@gmail.com>
2023-06-18 09:42:57 +0200

    Controller, Model, Factory, Routes

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Profile $profile)
+    {
+        //
+    }
+
+    public function update(Request $request, Profile $profile)
+    {
+        //
+    }
+
+    public function destroy(Profile $profile)
+    {
+        //
+    }
+}
