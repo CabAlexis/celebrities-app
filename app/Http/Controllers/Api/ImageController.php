3a6c826f9d693af1be6e2dadb1c9d4edad5c2792
CabAlexis <alexis.cabillic@gmail.com>
2023-08-14 00:14:30 +0200

    changes review

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UploadImageRequest;
+use Illuminate\Http\JsonResponse;
 
 class ImageController extends Controller
 {
-    public function uploadImage(UploadImageRequest $request)
+    public function uploadImage(UploadImageRequest $request) :JsonResponse
     {
         $vdata = $request->validated();
 
